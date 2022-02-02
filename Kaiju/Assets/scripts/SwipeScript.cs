d5056cfc2ee7d6292be846e0c9c364ce8689809c
Gustav Cleveman <angrykid_bum@hotmail.com>
2022-02-02 22:17:23 +0100

    swiping

--- Kaiju/Assets/scripts/SwipeScript.cs
+++ Kaiju/Assets/scripts/SwipeScript.cs
@@ -2,17 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class SwipeScript : MonoBehaviour, IDragHandler
+public class SwipeScript : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 _initialPosition;
+    private float _distanceMoved;
+    private bool _swipedLeft;
+    private int _cardIndex = 0;
+
+    public Sprite[] cards;
+
+
+
+
+    private void Start()
     {
-        
+        gameObject.GetComponent<Image>().sprite = cards[_cardIndex];
     }
+
 
     public void OnDrag(PointerEventData eventData)
     {
+
+        transform.localPosition = new Vector2(transform.localPosition.x + eventData.delta.x, transform.localPosition.y + eventData.delta.y);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _initialPosition = transform.localPosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _distanceMoved = Mathf.Abs(transform.localPosition.x - _initialPosition.x);
+
+        if (_distanceMoved < 0.15 * Screen.width)
+        {
+            transform.localPosition = _initialPosition;
+        }
+        else
+        {
+            if (transform.localPosition.x > _initialPosition.x)
+            {
+                _swipedLeft = false;
+            }
+            else
+            {
+                _swipedLeft = true;
+            }
+            StartCoroutine(MovedCard());
+        }
+
+    }
+
+    private IEnumerator MovedCard()
+    {
+        float time = 0;
+
+        //medan kortet inte är genomskinligt.
+        while (GetComponent<Image>().color != new Color(1, 1, 1, 0))
+        {
+            time += Time.deltaTime;
+
+            if (_swipedLeft)
+            {
+                //Avsluta påbörjad swipe.
+                transform.localPosition = new Vector3(Mathf.SmoothStep(transform.localPosition.x, transform.localPosition.x - Screen.width, 4 * time), transform.localPosition.y, 0);
+
+            }
+            else
+            {
+                transform.localPosition = new Vector3(Mathf.SmoothStep(transform.localPosition.x, transform.localPosition.x + Screen.width, 4 * time), transform.localPosition.y, 0);
+            }
+
+            //Gör kortet genomskinligt
+            GetComponent<Image>().color = new Color(1, 1, 1, Mathf.SmoothStep(1, 0, 4 * time));
+            yield return null;
+        }
+
+        _cardIndex++;
+        if (_cardIndex > cards.Length - 1)
+        {
+            _cardIndex = 0;
+        }
+
+        gameObject.GetComponent<Image>().sprite = cards[_cardIndex];
+        gameObject.transform.localPosition = _initialPosition;
+        GetComponent<Image>().color = new Color(1, 1, 1, 1);
 
     }
 }
